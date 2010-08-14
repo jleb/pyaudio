dda3aff3d3a576924f27b2e29ef5b823aee50db3
Hubert Pham <hubert@mit.edu>
2010-08-14 16:59:57 -0400

    Update version number, installation instructions, and changelog.

--- src/pyaudio.py
+++ src/pyaudio.py
@@ -1,6 +1,6 @@
 # PyAudio : Python Bindings for PortAudio.
 
-# Copyright (c) 2006-2008 Hubert Pham
+# Copyright (c) 2006-2010 Hubert Pham
 
 # Permission is hereby granted, free of charge, to any person obtaining
 # a copy of this software and associated documentation files (the
@@ -89,7 +89,7 @@
 """
 
 __author__ = "Hubert Pham"
-__version__ = "0.2.3"
+__version__ = "0.2.4"
 __docformat__ = "restructuredtext en"
 
 import sys

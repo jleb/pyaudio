7a610802700e8bbe13fb81682d65ea6c7a3baf2a
Hubert Pham <hubert@mit.edu>
2017-01-08 12:18:07 -0800

    Add duration for wire callback example.

--- examples/wire_callback.py
+++ examples/wire_callback.py
@@ -12,6 +12,7 @@ import sys
 WIDTH = 2
 CHANNELS = 2
 RATE = 44100
+DURATION = 5
 
 if sys.platform == 'darwin':
     CHANNELS = 1
@@ -30,7 +31,9 @@ stream = p.open(format=p.get_format_from_width(WIDTH),
 
 stream.start_stream()
 
-while stream.is_active():
+start = time.time()
+
+while stream.is_active() and (time.time() - start) < DURATION:
     time.sleep(0.1)
 
 stream.stop_stream()
